603acd93283ee118eadec1262083f3301154dea1
unknown <psasa@gmail.com.com>
2018-02-26 16:31:47 +0100

    260218

--- client/src/index.js
+++ client/src/index.js
@@ -9,17 +9,33 @@ import App from './components/app';
 import Signin from './components/auth/signin'
 import Signout from './components/auth/signout'
 import Signup from './components/auth/signup'
+import Feature from './components/feature'
+import Welcome from './components/Welcome'
+import requireAuth from './components/auth/requireAuth'
 import reducers from './reducers';
+import { AUTH_USER } from './actions/types'
 
 const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
+const store = createStoreWithMiddleware(reducers);
+
+const token = localStorage.getItem('token');
+// if we have a token, user is signed in
+
+if(token) {
+  // we need to update state
+  store.dispatch({ type: AUTH_USER })
+}
+
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
+  <Provider store={store}>
   <Router history={browserHistory}>
     <Route path="/" component={App}>
+    <IndexRoute component={Welcome}/>
       <Route path="signin" component={Signin} />
       <Route path="signout" component={Signout} />
-      <Route path="signup" component={Signup} />      
+      <Route path="signup" component={Signup} />    
+      <Route path="feature" component={requireAuth(Feature)} />  
     </Route>
   </Router>
   </Provider>
